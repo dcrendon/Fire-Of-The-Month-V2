2492d8304b362cc9d743e9669d15c505f7cce6e9
dcrendon <daniel.rendonfc@gmail.com>
2023-06-15 08:07:44 -0500

    mobile style updates

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ audio.addEventListener('ended', () => {
 })
 
 songInfo.innerHTML = `
-<article class="relative isolate overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80 w-full md:w-1/2">
+<article class="relative isolate overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-60 sm:pt-48 lg:pt-60 w-full md:w-1/2">
   <img src="https://i.ytimg.com/vi/${latestSong.youtubeID}/maxresdefault.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
   <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
   <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
